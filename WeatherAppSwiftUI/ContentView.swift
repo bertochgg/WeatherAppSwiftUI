e7a860d1535b02dab8e93baf83d2d8ef33ff27ab
Cesar Humberto Grifaldo Garcia <cesar.grifaldo@elektra.com.mx>
2025-09-20 08:56:50 -0600

    Initial Commit

---
+++ WeatherAppSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Cesar Humberto Grifaldo Garcia on 20/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Cesar Humberto Grifaldo Garcia on 20/09/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
