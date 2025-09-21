e21368eaf696eea5c383bfd590bd78cea5f921d9
Cesar Humberto Grifaldo Garcia <cesar.grifaldo@elektra.com.mx>
2025-09-21 13:28:06 -0600

    Added dashboard summary data view

--- WeatherAppSwiftUI/Utilities/UIComponents/WeatherByHourCapsuleView.swift
+++ WeatherAppSwiftUI/Utilities/UIComponents/WeatherByHourCapsuleView.swift
@@ -7,70 +7,34 @@
 
 import SwiftUI
 
-import SwiftUI
-
 struct WeatherByHourCapsuleView: View {
-
     var body: some View {
-        GeometryReader { proxy in
-            let capsuleWidth = proxy.size.width * 0.154
-            let capsuleHeight = proxy.size.height * 0.173
-
-            VStack(spacing: 12) {
-                Text("12 AM")
-                Image(systemName: "cloud.fill")
-                Text("30%")
-                Text("19°")
-            }
-            .frame(width: capsuleWidth, height: capsuleHeight)
-            .background(
-                Capsule()
-                    .fill(.ultraThinMaterial)
-            )
-            .overlay(
-                Capsule()
-                    .stroke(
-                        LinearGradient(
-                            colors: [Color.white.opacity(0.5), Color.clear],
-                            startPoint: .topLeading,
-                            endPoint: .bottomTrailing
-                        ),
-                        lineWidth: 1
-                    )
-            )
-            .shadow(color: Color.black.opacity(0.2), radius: 4, x: 2, y: 2)
+        VStack(spacing: 12) {
+            Text("12 AM")
+            Image(systemName: "cloud.fill")
+            Text("30%")
+            Text("19°")
         }
+        .padding(.vertical, 18)
+        .padding(.horizontal, 10)
+        .background(
+            Capsule()
+                .fill(.ultraThinMaterial)
+        )
+        .overlay(
+            Capsule()
+                .stroke(
+                    LinearGradient(
+                        colors: [Color.white.opacity(0.5), Color.clear],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    ),
+                    lineWidth: 1
+                )
+        )
+        .shadow(color: Color.black.opacity(0.2), radius: 4, x: 2, y: 2)
     }
 }
-
-//struct WeatherByHourCapsuleViewv1: View {
-//    var body: some View {
-//        VStack(spacing: 12) {
-//            Text("12 AM")
-//            Image(systemName: "cloud.fill")
-//            Text("30%")
-//            Text("19°")
-//        }
-//        .padding(.vertical, 18)
-//        .padding(.horizontal, 10)
-//        .background(
-//            Capsule()
-//                .fill(.ultraThinMaterial)
-//        )
-//        .overlay(
-//            Capsule()
-//                .stroke(
-//                    LinearGradient(
-//                        colors: [Color.white.opacity(0.5), Color.clear],
-//                        startPoint: .topLeading,
-//                        endPoint: .bottomTrailing
-//                    ),
-//                    lineWidth: 1
-//                )
-//        )
-//        .shadow(color: Color.black.opacity(0.2), radius: 4, x: 2, y: 2)
-//    }
-//}
 
 
 #Preview {
---
+++ WeatherAppSwiftUI/Utilities/UIComponents/WeatherSummaryDataView.swift
@@ -0,0 +1,117 @@
+//
+//  WeatherSummaryDataView.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Cesar Humberto Grifaldo Garcia on 20/09/25.
+//
+
+import SwiftUI
+
+struct WeatherSummaryDataView<Content: View>: View {
+    @GestureState private var dragOffset = CGSize.zero
+    @State private var position: CGFloat? = nil
+    
+    var content: () -> Content
+    
+    var body: some View {
+        GeometryReader { proxy in
+            let screenHeight = proxy.size.height
+            let collapsed = screenHeight * 0.60
+            let expanded: CGFloat = 100
+            VStack {
+                Capsule()
+                    .fill(Color.black.opacity(0.3))
+                    .frame(width: 48, height: 5)
+                    .padding(.top, 8)
+                HStack(spacing: 96) {
+                    Button("Hourly Forecast") {
+                        
+                    }
+                    .font(.system(size: 15))
+                    .fontWeight(.semibold)
+                    .foregroundStyle(Color(red: 235/255, green: 235/255, blue: 245/255)).opacity(0.6)
+                    Button("Weekly Forecast") {
+                        
+                    }
+                    .font(.system(size: 15))
+                    .fontWeight(.semibold)
+                    .foregroundStyle(Color(red: 235/255, green: 235/255, blue: 245/255)).opacity(0.6)
+                }
+                Divider()
+                    .overlay(
+                        LinearGradient(
+                            colors: [Color.white.opacity(0.7), Color.white.opacity(0.2)],
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                        .frame(height: 1),
+                        alignment: .bottom
+                    )
+                content()
+                Spacer()
+            }
+            .frame(maxWidth: .infinity)
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(.ultraThinMaterial)
+            .cornerRadius(48)
+            .border(LinearGradient(
+                colors: [Color.white, Color.white],
+                startPoint: .topLeading,
+                endPoint: .topTrailing
+            ), width: 1).cornerRadius(48)
+                .offset(y: (position ?? collapsed) + dragOffset.height)
+                .gesture(
+                    DragGesture()
+                        .updating($dragOffset) { value, state, _ in
+                            state = value.translation
+                        }
+                        .onEnded { value in
+                            if value.translation.height < -100 {
+                                position = expanded
+                            } else {
+                                position = collapsed
+                            }
+                        }
+                )
+                .animation(.spring(), value: position)
+                .onAppear {
+                    position = collapsed
+                }
+        }
+        .ignoresSafeArea(edges: .bottom)
+    }
+}
+
+#Preview {
+    ZStack {
+        LinearGradient(colors: [Color.purple, Color.blue], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .ignoresSafeArea()
+        WeatherSummaryDataView {
+            ScrollView(.vertical) {
+                VStack {
+                    ScrollView(.horizontal) {
+                        HStack(spacing: 12) {
+                            WeatherByHourCapsuleView()
+                            WeatherByHourCapsuleView()
+                            WeatherByHourCapsuleView()
+                            WeatherByHourCapsuleView()
+                            WeatherByHourCapsuleView()
+                            WeatherByHourCapsuleView()
+                            WeatherByHourCapsuleView()
+                            WeatherByHourCapsuleView()
+                            WeatherByHourCapsuleView()
+                            WeatherByHourCapsuleView()
+                            WeatherByHourCapsuleView()
+                            WeatherByHourCapsuleView()
+                            WeatherByHourCapsuleView()
+                        }
+                        .padding(.leading, 20)
+                        .padding(.trailing, 20)
+                    }
+                }
+            }
+            .padding(.top, 19)
+            .padding(.bottom, 100)
+        }
+    }
+}
