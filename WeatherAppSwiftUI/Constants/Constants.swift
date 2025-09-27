e255a913a3a409b84cb9ed9e6a5c30fa4bd19fa1
Cesar Humberto <bertochgg@gmail.com>
2025-09-27 16:27:20 -0600

    Some commit

--- WeatherAppSwiftUI/Constants/Constants.swift
+++ WeatherAppSwiftUI/Constants/Constants.swift
@@ -7,6 +7,6 @@
 
 struct Constants {
     struct Keys {
-        static let openWeatherApiKey: String = "7d0977c2966e8677c9625a4397c504d6"
+        static let openWeatherApiKey: String = ""
     }
 }
